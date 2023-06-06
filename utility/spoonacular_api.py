f26935796f3a3f33eea9d54733b7531e6c74c97b
patrick <patrickkwon777@gmail.com>
2023-06-06 23:56:12 +0700

    fix bug

--- utility/spoonacular_api.py
+++ utility/spoonacular_api.py
@@ -2,11 +2,7 @@
 
 
 keys = [
-<<<<<<< HEAD
     "5bc88ee9e9e94669b308500cd72e73c5",
-=======
-    "d435f6da0b66438f9f4f76edbf4e895e",
->>>>>>> ebc6270c23692be6defca00669fa32c260254bec
     ]
 index = 0
 def get_key():
